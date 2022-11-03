90700db6b94ed1b1c97bfe076f99f08549358b69
Oliver <oliver.gruendel2004@gmail.com>
2022-11-03 23:03:56 +0100

    Best Friends

---
+++ Python/BestFriends.py
@@ -0,0 +1,17 @@
+# Erstellen der Variable 'freunde' als Listen Objekt
+freunde = ['Freund 1', 'Freund 2', 'Freund 3']
+
+# Drucken der Liste 'freunde' in die Konsole
+print(freunde)
+
+# Löschen des ersten Elements der Liste 'freunde'
+freunde.pop(0)
+
+# Drucken der geänderten Liste 'freunde' in die Konsole
+print(freunde)
+
+# Hinzufügen der Freundin Uschi in die Liste 'freunde'
+freunde.append('Uschi')
+
+# Drucken der geänderten Liste 'freunde' in die Konsole
+print(freunde)
