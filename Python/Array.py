d787662dd151091b92e558a1f94f7400341148e6
Oliver <oliver.gruendel2004@gmail.com>
2022-11-07 17:52:51 +0100

    Array

---
+++ Python/Array.py
@@ -0,0 +1,8 @@
+# Erstellen der Variable 'liste' als Listen Objekt
+liste = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Umkehren der Reihenfolge der Elemente in der Liste 'liste'
+liste.reverse()
+
+# Drucken der Variable 'liste' in die Konsole
+print(liste)
