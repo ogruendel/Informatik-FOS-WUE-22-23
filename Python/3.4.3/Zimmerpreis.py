9cc1da09bb82eb36ee0b59f047ef5a11e2b6e247
Oliver <oliver.gruendel2004@gmail.com>
2022-11-08 17:07:22 +0100

    Zimmerpreis

--- Python/3.4.3/Zimmerpreis.py
+++ Python/3.4.3/Zimmerpreis.py
@@ -1,6 +1,7 @@
 personen = int(input("Anzahl Personen: "))
+nächte = int(input("Anzahl Nächte: "))
 
 if personen % 2 != 0:
-    print(format(((personen - 1) / 2) * 70 + 50, '.2f') + "€")
+    print(format((((personen - 1) / 2) * 70 + 50) * nächte, '.2f') + "€")
 else:
-    print(format(personen / 2 * 70, '.2f') + "€")
+    print(format((personen / 2 * 70) * nächte, '.2f') + "€")
