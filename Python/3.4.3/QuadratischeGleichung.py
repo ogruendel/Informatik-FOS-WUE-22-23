3876583b686cf7882c583ad88be2002cc0a05a66
Oliver <oliver.gruendel2004@gmail.com>
2022-11-08 16:19:03 +0100

    Quadratische Gleichung

--- Python/3.4.3/QuadratischeGleichung.py
+++ Python/3.4.3/QuadratischeGleichung.py
@@ -4,15 +4,15 @@ a = float(input("a = "))
 b = float(input("b = "))
 c = float(input("c = "))
 
-x1 = (-b + math.sqrt((b * b) - (4 * a * c))) / 2 * a
-x2 = (-b - math.sqrt((b * b) - (4 * a * c))) / 2 * a
-
 if b * b - (4 * a * c) == 0:
     print("Eine Lösung:")
-    print("x =", x1)
 elif b * b - (4 * a * c) < 0:
     print("Keine Lösung!")
 else:
     print("Zwei Lösungen:")
+
+    x1 = (-b + (math.sqrt((b * b) - (4 * a * c)))) / (2 * a)
+    x2 = (-b - (math.sqrt((b * b) - (4 * a * c)))) / (2 * a)
+
     print("x1 =", x1)
     print("x2 =", x2)
