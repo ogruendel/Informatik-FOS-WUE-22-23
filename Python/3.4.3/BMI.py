31189efb14d5d6bac7c2fa894ecf7fa598f5c8ca
Oliver <oliver.gruendel2004@gmail.com>
2022-11-08 16:31:00 +0100

    BMI

---
+++ Python/3.4.3/BMI.py
@@ -0,0 +1,20 @@
+größe = float(input("Größe in cm: ")) / 100
+gewicht = float(input("Gewicht in Kilogramm: "))
+geschlecht = input("Geschlecht? (m) oder (f): ")
+
+bmi = round(gewicht / (größe * größe), 1)
+
+if geschlecht == "m":
+    if bmi < 20:
+        print("BMI:", bmi, "Untergewicht")
+    elif bmi > 25:
+        print("BMI:", bmi, "Übergewicht")
+    else:
+        print("BMI:", bmi, "Normalgewicht")
+else:
+    if bmi < 19:
+        print("BMI:", bmi, "Untergewicht")
+    elif bmi > 24:
+        print("BMI:", bmi, "Übergewicht")
+    else:
+        print("BMI:", bmi, "Normalgewicht")
