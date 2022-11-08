cb54ce63824e0a42d9f4e56186746ac2d92a6f72
Oliver <oliver.gruendel2004@gmail.com>
2022-11-08 16:52:16 +0100

    Größer

---
+++ Python/3.4.3/Größer.py
@@ -0,0 +1,44 @@
+# a = int(input("Zahl 1: "))
+# b = int(input("Zahl 2: "))
+# c = int(input("Zahl 3: "))
+
+a = 4
+b = 3
+c = 4
+
+# TODO: Optimieren
+
+if a >= b:
+    if a >= c:
+        if c >= a:
+            if b >= c:
+                print("A, B und C sind gleich groß")
+                exit()
+            print("A und C sind gleich groß")
+            exit()
+    if b >= c:
+        if b >= a:
+            if a >= b:
+                print("A und B sind gleich groß")
+                exit()
+            print("B ist am größten")
+            exit()
+        print("A ist am größten")
+        exit()
+    if c >= b:
+        if c >= a:
+            print("C ist am größten")
+            exit()
+        print("B ist am größten")
+        exit()
+    print("A ist am größten")
+    exit()
+elif b >= a:
+    if c >= b:
+        if b >= c:
+            print("B und C sind gleich groß")
+            exit()
+        print("C ist am größten")
+        exit()
+    print("B ist am größten")
+    exit()
