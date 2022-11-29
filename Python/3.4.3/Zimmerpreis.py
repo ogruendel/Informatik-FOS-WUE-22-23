976463718d3c37ad006141b8e8fa8348ba2f68cc
ogruendel <oliver.gruendel2004@gmail.com>
2022-11-29 10:25:55 +0100

    Zimmerpreis

--- Python/3.4.3/Zimmerpreis.py
+++ Python/3.4.3/Zimmerpreis.py
@@ -1,7 +1,14 @@
+# Das Programm soll den Zimmerpreis pro Nacht ermitteln. Einzelzimmer kosten 50 € pro Nacht, Doppelzimmer
+# kosten 70 €. Vom Nutzer soll abgefragt werden, wie viele Personen ein Zimmer benötigen. Dabei sollen
+# immer zwei Personen in einem Zimmer übernachten. Bei einer ungeraden Anzahl von Personen wird einem
+# automatisch ein Einzelzimmer zugewiesen.
+
 personen = int(input("Anzahl Personen: "))
-nächte = int(input("Anzahl Nächte: "))
+naechte = int(input("Anzahl Nächte: "))
 
 if personen % 2 != 0:
-    print(format((((personen - 1) / 2) * 70 + 50) * nächte, '.2f') + "€")
+    preis = ((personen - 1) / 2) * 70 + 50
+    print(format(preis * naechte, '.2f') + "€")
 else:
-    print(format((personen / 2 * 70) * nächte, '.2f') + "€")
+    preis = (personen / 2) * 70
+    print(format(preis * naechte, '.2f') + "€")
