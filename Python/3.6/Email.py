2a709062961e0c31fa3417ec8dbb29a1b634771a
ogruendel <oliver.gruendel2004@gmail.com>
2023-01-10 11:08:40 +0100

    Email

--- Python/3.6/Email.py
+++ Python/3.6/Email.py
@@ -1,2 +1,4 @@
-mail = input("Was ist die Email Adresse: ")
+mail = "Tonys-Tiernahrung@web.de"
+mail2 = "info@Wellesittich-Rudy.de"
 print(mail.split('@')[0])
+print(mail2.split('.')[0].split('@')[1])
