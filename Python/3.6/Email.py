73d5b079c321e2d6ec42207b9fb697aeaaacc33f
ogruendel <oliver.gruendel2004@gmail.com>
2023-01-17 09:49:35 +0100

    Email

--- Python/3.6/Email.py
+++ Python/3.6/Email.py
@@ -1,4 +1,10 @@
 mail = "Tonys-Tiernahrung@web.de"
 mail2 = "info@Wellesittich-Rudy.de"
+lieferanten = []
+lieferant_neu = ["hundebedarf", "wuerzburg.com"]
 print(mail.split('@')[0])
 print(mail2.split('.')[0].split('@')[1])
+lieferanten.append(mail)
+lieferanten.append(mail2)
+lieferanten.append(lieferant_neu[0] + "@" + lieferant_neu[1])
+print(lieferanten)
