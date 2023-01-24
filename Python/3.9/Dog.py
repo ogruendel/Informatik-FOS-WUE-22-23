cd022162ba11bcb84b56d56bc0b8aeac00bc5d1e
ogruendel <oliver.gruendel2004@gmail.com>
2023-01-24 09:55:32 +0100

    Dog

---
+++ Python/3.9/Dog.py
@@ -0,0 +1,10 @@
+def hundefutter(name, weight):
+    if weight > 20:
+        print(f'{name} bekommt Futter XXL')
+    else:
+        print(f'{name} bekommt Futter Mini')
+
+
+hundefutter("Sparky", 30)
+hundefutter("Lilly", 10)
+hundefutter("Caeser", 15)
