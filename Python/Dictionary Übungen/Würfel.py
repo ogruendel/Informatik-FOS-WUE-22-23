9f68b79d1ca12bc8e3bb9890355af1143a68c9cf
ogruendel <oliver.gruendel2004@gmail.com>
2023-03-14 10:16:29 +0100

    Würfel

--- Python/Dictionary Übungen/Würfel.py
+++ Python/Dictionary Übungen/Würfel.py
@@ -1,13 +1,12 @@
 import random
 
-d = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+d = {2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0}
 
 for i in range(0, 1000):
     v1 = random.randint(1, 6)
     v2 = random.randint(1, 6)
 
-    d[v1] += 1
-    d[v2] += 1
+    d[v1 + v2] += 1
 
 for k, i in d.items():
     print(k, i)
